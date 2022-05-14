8fa94739ec5d40b9b3e19490d43f14f2b9a8122c
LucasRDDalMas <lucasrddalmas@gmail.com>
2022-05-13 21:36:05 -0300

    feat(wip): create login - create login form

--- src/presentation/pages/Login/index.tsx
+++ src/presentation/pages/Login/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
-import { Footer, Main, MainImage, MainLoginForm, MainWrapper } from './styles'
-import login from '../../assets/images/login.png'
+import { Apps, AppsLinks, Footer, ForgotPassword, LoginForm, LoginFormButton, LoginFormInput, LoginLogo, LoginSocial, LoginWrapper, Main, MainImage, MainLoginWrapper, MainWrapper, SignUpWrapper } from './styles'
+import LoginImage from '../../assets/images/login.png'
+import GooglePlay from '../../assets/images/google-play.png'
+import AppStore from '../../assets/images/app-store.png'
+import { MainLogo } from '@/presentation/assets/logos'
+import { Divider } from '@/presentation/components/shared/styles'
+import { IconFacebook } from '@/presentation/assets/icons'
 
 const Login: React.FC = () => {
   return (
@@ -8,9 +13,45 @@ const Login: React.FC = () => {
       <MainWrapper>
         <Main>
           <MainImage>
-            <img src={login} alt='' />
+            <img src={LoginImage} alt='' />
           </MainImage>
-          <MainLoginForm>a</MainLoginForm>
+          <MainLoginWrapper>
+            <LoginWrapper>
+              <LoginLogo>
+                <img src={MainLogo} alt=''/>
+              </LoginLogo>
+              <LoginForm>
+                <LoginFormInput type='text' name='' value='' placeholder='' />
+                <LoginFormInput type='password' name='' value='' placeholder='' />
+                <LoginFormButton>Log In</LoginFormButton>
+              </LoginForm>
+
+              <Divider>Or</Divider>
+
+              <LoginSocial>
+                <IconFacebook size='24' color='#385184' />
+                <span>Log in with Facebook</span>
+              </LoginSocial>
+
+              <ForgotPassword>
+                Forgot password?
+              </ForgotPassword>
+            </LoginWrapper>
+            <SignUpWrapper>
+              Don&apos;t have an account?&nbsp;<a href=''>Sign up</a>
+            </SignUpWrapper>
+            <Apps>
+              <p>Get the app.</p>
+              <AppsLinks>
+                <a href=''>
+                  <img src={GooglePlay} alt='' />
+                </a>
+                <a href=''>
+                  <img src={AppStore} alt='' />
+                </a>
+              </AppsLinks>
+            </Apps>
+          </MainLoginWrapper>
         </Main>
       </MainWrapper>
       <Footer></Footer>
--- src/presentation/pages/Login/styles.tsx
+++ src/presentation/pages/Login/styles.tsx
@@ -15,6 +15,7 @@ export const Main = styled.article`
   flex-grow: 1;
 
   height: 582px;
+  justify-content: center;
   flex-direction: row;
 `
 
@@ -23,19 +24,131 @@ export const MainImage = styled.div`
   flex-basis: 380px;
   height: 100%;
   margin-right: 32px;
-  background-color: blue;
+  justify-content: center;
 
   img {
     height: 100%;
   }
 `
 
-export const MainLoginForm = styled.div`
+export const MainLoginWrapper = styled.div`
   display: flex;
   justify-content: center;
+  flex-direction: column;
   max-width: 350px;
+  height: 100%;
   flex-grow: 1;
-  background-color: red;
+  gap: 10px;
+`
+
+export const LoginWrapper = styled.div`
+  border: 1px solid var(--border);
+  background-color: var(--background);
+  padding: 10px 40px;
+`
+
+export const LoginLogo = styled.div`
+  display: flex;
+  flex: 0 0 auto;
+  margin: 36px 0 12px;
+  height: 56px;
+
+  img {
+    max-height: 100%;
+    max-width: 100%;
+    object-fit: contain;
+  }
+`
+
+export const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding-top: 24px;
+`
+
+export const LoginFormInput = styled.input`
+  border: 1px solid var(--border);
+  border-radius: 3px;
+  padding: 9px 0 7px 8px;
+  margin-bottom: 6px;
+`
+
+export const LoginFormButton = styled.button`
+  display: flex;
+  flex: 0 0 auto;
+  background-color: var(--quartary);
+  color: rgb(var(--white));
+  margin: 8px 0;
+  font-weight: 600;
+  padding: 5px 9px;
+  justify-content: center;
+  border-radius: 4px;
+  border: 1px solid transparent;
+  font-size: 14px;
+`
+
+export const LoginSocial = styled.button`
+  display: flex;
+  width: 100%;
+  margin: 8px 0;
+  color: #385185;
+  margin: 8px 0;
+  font-weight: 600;
+  padding: 5px 9px;
+  justify-content: center;
+  align-items: center;
+
+  span {
+    margin-left: 8px;
+    font-weight: 600;
+    font-size: 14px;
+  }
+`
+
+export const ForgotPassword = styled.a`
+  color: #00376b;;
+  font-size: 12px;
+  line-height: 16px;
+  margin-top: 12px;
+  text-align: center;
+  display: block;
+  width: 100%;
+`
+
+export const SignUpWrapper = styled.div`
+  border: 1px solid var(--border);
+  background-color: var(--background);
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  padding: 25px 0;
+  border-radius: 1px;
+
+  a {
+    font-weight: 600;
+  }
+`
+
+export const Apps = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  p {
+    margin: 10px 20px 10px 20px;
+  }
+`
+
+export const AppsLinks = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 10px 0;
+  gap: 10px;
+
+  img {
+    height: 40px;
+  }
 `
 
 export const Footer = styled.footer`
